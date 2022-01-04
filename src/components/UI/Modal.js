f40bf5f9d40d15ad7d3493152b0cb5b28ac01d24
hagleyson <hagleyson@gmail.com>
2022-01-04 14:24:51 -0300

    feat add modal

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,5 +1,27 @@
+import { createPortal } from "react-dom";
 import classes from "./Modal.module.css";
+const Backdrop = (props) => {
+  return <div onClick={props.onClose} className={classes.backdrop} />;
+};
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
 
-const Modal = (props) => {};
+const portalElement = document.getElementById("overlays");
+const Modal = (props) => {
+  return (
+    <>
+      {createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
+      {createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </>
+  );
+};
 
 export default Modal;
